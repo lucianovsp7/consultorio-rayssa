8f1ac67d5a92f9ff7c029ee57649be3ef9a37d2e
Luciano Pereira <lucianopereira@Lucianos-MacBook-Pro.local>
2026-03-18 11:08:32 -0300

    fix render port

--- app.py
+++ app.py
@@ -79,5 +79,8 @@ def limpar():
 # =========================
 # RODAR
 # =========================
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
